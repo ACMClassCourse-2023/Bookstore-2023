2193019791674aa3bee353f08e18aa7f724a0be3
skywong14 <skywong0014@gmail.com>
2023-12-14 15:31:55 +0800

    modify CMakeLists.txt

--- src/memoryio.h
+++ src/memoryio.h
@@ -35,7 +35,7 @@ class Memory {
 private:
     fstream file;
     string file_name,file_index,file_pool,file_value;
-    const int block_size = 300;
+    const int block_size = 1000;
     const long long BASE = 131, MOD = 1e9+7;
 
     int sizeofT = sizeof(T);
